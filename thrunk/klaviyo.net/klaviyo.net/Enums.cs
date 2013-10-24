7ac73e249aebff13e4d5d1f2ff131df8bfec78f9
Aleksey Fomin <fomin_aa@bk.ru>
2013-10-24 19:07:40 +0700

    first version klaviyo

---
+++ thrunk/klaviyo.net/klaviyo.net/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klaviyo.net
+{
+    public enum SubmitStatus
+    {
+        Fail,
+        Success
+    }
+}
---
+++ thrunk/klaviyo.net/klaviyo.net/KlaviyoGateway.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace klaviyo.net
+{
+    public class KlaviyoGateway
+    {
+        private Uri _baseAddressUri = new Uri("http://a.klaviyo.com/api/");
+
+        private string _token = "";
+
+        public KlaviyoGateway(string token)
+        {
+            _token = token;
+        }
+
+        public string Token
+        {
+            get;
+            set;
+        }
+
+        public SubmitStatus PostEvent(KlaviyoEvent obj)
+        {
+            using (WebClient downloader = new WebClient())
+            {
+                UriTemplate uriTemplate = new UriTemplate("track?data={data}");
+                IDictionary<string, string> parameters = new Dictionary<string, string>();
+
+                System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+                byte[] bytes = encoding.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(obj));
+                string sBase64 = System.Convert.ToBase64String(bytes);
+                parameters.Add("data", sBase64);
+                Uri formattedUri = uriTemplate.BindByName(_baseAddressUri, parameters);
+                string str = "";
+
+                using (Stream myStream = downloader.OpenRead(formattedUri))
+                {
+                    using (StreamReader sr = new StreamReader(myStream))
+                    {
+                        str = sr.ReadToEnd();
+                    }
+                }
+
+                if (str == "1") return SubmitStatus.Success;
+                else return SubmitStatus.Fail;
+            }
+
+        }
+    }
+}
